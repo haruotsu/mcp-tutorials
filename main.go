cb372cf3c17655a57ff1478f40cfd2e2663bb20f
haruotsu <yoko.haruru.phy@outlook.jp>
2025-04-06 20:47:02 +0900

    いったん無事動いた

--- main.go
+++ main.go
@@ -1,7 +1,61 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	"github.com/mark3labs/mcp-go/mcp"
+	"github.com/mark3labs/mcp-go/server"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	s := server.NewMCPServer(
+		"Calculator Demo",
+		"1.0.0",
+		server.WithResourceCapabilities(true, true), // Resource の機能で使われるオプションなのでToolの公開のみであれば不要そう
+		server.WithLogging(),
+	)
+	// 四則計算ツールのインターフェース登録
+	calculatorTool := mcp.NewTool("calculate",
+		mcp.WithDescription("Perform basic arithmetic operations"),
+		mcp.WithString("operation",
+			mcp.Required(),
+			mcp.Description("The operation to perform (add, subtract, multiply, divide)"),
+			mcp.Enum("add", "subtract", "multiply", "divide"),
+		),
+		mcp.WithNumber("x",
+			mcp.Required(),
+			mcp.Description("First number"),
+		),
+		mcp.WithNumber("y",
+			mcp.Required(),
+			mcp.Description("Second number"),
+		),
+	)
+
+	// 四則計算ツールを実装
+	s.AddTool(calculatorTool, func(ctx context.Context, request mcp.CallToolRequest) (*mcp.CallToolResult, error) {
+		op := request.Params.Arguments["operation"].(string)
+		x := request.Params.Arguments["x"].(float64)
+		y := request.Params.Arguments["y"].(float64)
+
+		var result float64
+		switch op {
+		case "add":
+			result = x + y
+		case "subtract":
+			result = x - y
+		case "multiply":
+			result = x * y
+		case "divide":
+			return mcp.NewToolResultError("未対応の機能です"), nil
+		}
+
+		return mcp.NewToolResultText(fmt.Sprintf("%.2f", result)), nil
+	})
+
+	// サーバー起動
+	if err := server.ServeStdio(s); err != nil {
+		fmt.Printf("Server error: %v\n", err)
+	}
 }
